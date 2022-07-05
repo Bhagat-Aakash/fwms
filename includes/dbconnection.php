e722369b7303ac187d2ea8aa92008864249897c3
Bhagat-Aakash <bhagataakash250@gmail.com>
2022-07-05 22:15:05 +0530

    DB Connection

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "fwmsdb");
+$con=mysqli_connect("sql3.freesqldatabase.com", "sql3504217", "3CPE9B5MXJ", "sql3504217");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
